f730b552cc4c29174523601c794da0993f73876c
William Wold <wm@wmww.sh>
2022-04-21 16:13:17 -0400

    Create and destroy symlink to frame Wayland display in tmp

--- src/frame_main.cpp
+++ src/frame_main.cpp
@@ -28,6 +28,18 @@
 #include <miral/set_window_management_policy.h>
 #include <miral/wayland_extensions.h>
 
+#include <filesystem>
+
+auto display_link_path(miral::MirRunner& runner) -> std::string
+{
+    return "/tmp/frame-display-" + runner.wayland_display().value();
+}
+
+auto display_path(miral::MirRunner& runner) -> std::string
+{
+    return std::string{getenv("XDG_RUNTIME_DIR")} + "/" + runner.wayland_display().value();
+}
+
 int main(int argc, char const* argv[])
 {
     using namespace miral;
@@ -36,6 +48,19 @@ int main(int argc, char const* argv[])
     DisplayConfiguration display_config{runner};
     WaylandExtensions wayland_extensions;
     init_authorization(wayland_extensions);
+
+    // Symlinking frame's display from our private /tmp allows clients from this snap to choose the
+    // correct display if, for example, we're running nested under another Wayland compositor.
+    runner.add_start_callback([&]
+        {
+            auto const target = display_path(runner);
+            auto const link = display_link_path(runner);
+            std::filesystem::create_symlink(target, link);
+        });
+    runner.add_stop_callback([&]
+        {
+            remove(display_link_path(runner).c_str());
+        });
 
     egmde::Wallpaper wallpaper;
     runner.add_stop_callback([&] { wallpaper.stop(); });
