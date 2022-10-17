cb08fc1d0aa7dd618cb7f7b2072c3090be30d025
Michał Sawicz (Saviq) <michal@sawicz.net>
2022-10-17 16:38:58 +0200

    copyright: fix years

--- src/frame_window_manager.cpp
+++ src/frame_window_manager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016-2021 Canonical Ltd.
+ * Copyright © 2016-2022 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 or 3 as
