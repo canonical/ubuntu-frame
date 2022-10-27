1a5d6c8671d1dbbca5071c7bd4e55e50aa5544c4
Grayson Guarino <graysonguarino@gmail.com>
2022-10-27 12:15:24 -0500

    Seperate codepaths for relative and absolute

--- src/background_client.cpp
+++ src/background_client.cpp
@@ -173,15 +173,29 @@ void BackgroundClient::set_diagnostic_path(std::string const& option)
         return;
     }
     
-    auto option_path = Path(option);
-    if (exists(option_path.parent_path()))
+    if (option.at(0) == '/')
     {
-        diagnostic_path = option_path;
+        auto const absolute_path = Path(option);
+        if (exists(absolute_path.parent_path()) && !boost::filesystem::is_directory(absolute_path))
+        {
+            diagnostic_path = absolute_path;
+            return;
+        }
+
+        BOOST_THROW_EXCEPTION(std::runtime_error(
+            "Diagnostic path (" + option + ") is invalid"));
     }
     else
     {
+        auto const relative_path = boost::filesystem::current_path().append(option);
+        if (exists(relative_path.parent_path()) && !boost::filesystem::is_directory(relative_path))
+        {
+            diagnostic_path = relative_path;
+            return;
+        }
+
         BOOST_THROW_EXCEPTION(std::runtime_error(
-            "Diagnostic path (" + option_path.parent_path().string() + ") does not exist"));
+            "Diagnostic path (" + relative_path.string() + ") is invalid"));
     }
 }
 
