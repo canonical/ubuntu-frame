0337ef18e3a11319e55ae893dfc697812b59c432
Matthew Kosarek <matthew@matthewkosarek.xyz>
2024-06-24 13:28:37 -0400

    rename run-client-wait to wayland-launch + license

--- launcher/linux/main.cc
+++ launcher/linux/main.cc
@@ -1,6 +1,22 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "my_application.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
