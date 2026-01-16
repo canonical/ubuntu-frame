9051cd2f8402a8f17932ab034b18ad228b2fd31e
Michal Sawicz <michal.sawicz@canonical.com>
2026-01-16 17:18:25 +0100

    doc: ignore freedesktop linkcheck issues

--- docs/conf.py
+++ docs/conf.py
@@ -240,6 +240,7 @@ linkcheck_ignore = [
     "https://ubuntu.com/.*#get-in-touch",
     "https://matrix.to/#/#mir-server:matrix.org",
     "https://www.realvnc.com/.*",
+    "https://www.freedesktop.org/", # has bot protections in place
     ]
 
 
