5be56430e9201471e350e246ccc6c86b4f6a0c59
Michal Sawicz <michal.sawicz@canonical.com>
2026-05-07 13:53:58 +0200

    docs: resolve redirected reverences

--- docs/conf.py
+++ docs/conf.py
@@ -385,10 +385,10 @@ intersphinx_mapping = {
         "https://canonical-example-product-documentation.readthedocs-hosted.com/en/latest",
         None,
     ),
-    "mir": ("https://canonical-mir.readthedocs-hosted.com/stable", None),
-    "mir-latest": ("https://canonical-mir.readthedocs-hosted.com/latest", None),
+    "mir": ("https://canonical.com/mir/stable", None),
+    "mir-latest": ("https://canonical.com/mir/latest", None),
     "core": ("https://documentation.ubuntu.com/core", None),
-    "frame-22": ("https://canonical-ubuntu-frame-documentation.readthedocs-hosted.com/22", None),
+    "frame-22": ("https://ubuntu.com/frame/docs/22", None),
     "server": ("https://documentation.ubuntu.com/server", None),
     "snapcraft": ("https://documentation.ubuntu.com/snapcraft/stable", None),
     "subiquity": (
